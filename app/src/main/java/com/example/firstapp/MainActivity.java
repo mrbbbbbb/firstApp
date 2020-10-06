f8e21ffd92d19de01021560b3b6233059302f6c4
mrbbbbbb <738391429@qq.com>
2020-10-06 17:16:58 +0800

    欢迎页和登录页面设计

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.firstapp.activity.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin,btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnRegister = findViewById(R.id.btn_register);
     }
 }
